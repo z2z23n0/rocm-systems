4eed50bc01605f8818dce8294af93e50380d5e56
Sam Crater <samuel.crater@amd.com>
2026-08-19 05:10:42 -0700

    comment update
    Co-authored-by: Vladimir Indic <139573562+vlaindic@users.noreply.github.com>

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/pc_sampling/utils.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/pc_sampling/utils.cpp
@@ -73,8 +73,8 @@ get_matching_hsa_pcs_units(rocprofiler_pc_sampling_unit_t unit)
 size_t
 get_hsa_pcs_buffer_size(uint32_t gfx_target_version)
 {
-    // Since ROCM-22213, the buffer is split per XCC, so we need larger buffers for these GPUs.
-    // For now, just enable for MI300/350/400 series chips.
+    // To increase the throughput and reduce the number of dropped samples, ROCr allocates a buffer per XCC.
+    // Thus, we increase the overall ROCr PC sampling buffer size for the multi-chiplet architectures like gfx942 and gfx1250.
     const auto is_multi_xcc = [](uint32_t gfx_version) -> bool {
         // gfx_target_version encodes major*10000 + minor*100 + patch
         switch(gfx_version / 100)
